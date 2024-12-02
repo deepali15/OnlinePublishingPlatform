f699b326b0b74511cab4032c2f0fa0a5b2949303
Deepali Deepali <deepalideepali@Deepalis-MacBook-Air.local>
2024-12-03 01:04:16 +0530

    rich text editor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyD55YCl8HjqXZctTYn7Qzh5sDx0NGs6zWM",
-    authDomain: "angular-login-52580.firebaseapp.com",
-    projectId: "angular-login-52580",
-    storageBucket: "angular-login-52580.firebasestorage.app",
-    messagingSenderId: "699950320244",
-    appId: "1:699950320244:web:bd2c51a977df0cefbadf21",
-    measurementId: "G-ZR8T85V9NL"
-  }
-};
+  apikey:"g29yfwp0i72mk2e2s1z9ufpmu1zr3xdnmqmbi6kdxs7x6z9t"
+  };
